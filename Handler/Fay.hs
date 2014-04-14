d80e008224a7e41af32b705f401888c64426888c
Willem van den Ende <willem@qwan.eu>
2014-04-14 11:38:05 +0100

    extracted handleBrowserFeaturesMethod
    Because I am trying to figure out how to do some IO on the server side, and was hoping ghci would give me a useful type signature (e.g. print or
    log the query, save the browser property in the database. I eventually want to use a record with more properties as parameter for the command,
    but am going for a vertical slice first).

--- Handler/Fay.hs
+++ Handler/Fay.hs
@@ -7,9 +7,12 @@ import Fay.Convert (readFromFay)
 fibs :: [Int]
 fibs = 0 : 1 : zipWith (+) fibs (drop 1 fibs)
 
+handleBrowserFeatures :: forall t t1 t2. (t2 -> Bool -> t1) -> t -> t2 -> t1
+handleBrowserFeatures render q r = render r $ False
+
 onCommand :: CommandHandler App
 onCommand render command =
     case readFromFay command of
       Just (GetFib index r) -> render r $ fibs !! index
-      Just (PostQuerySelector querySelector r) -> render r $ False
+      Just (PostQuerySelector querySelector r) -> handleBrowserFeatures render querySelector r
       Nothing               -> invalidArgs ["Invalid command"]
