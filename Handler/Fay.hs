becba3b2b0825532c2eed52e5073a40eab8cbfcd
Willem van den Ende <willem@qwan.eu>
2014-04-15 09:36:53 +0100

    extracted handleBrowserFeaturesMethod
    Because I am trying to figure out how to do some IO on the server side, and was hoping ghci would give me a useful type signature (e.g. print or
    log the query, save the browser property in the database. I eventually want to use a record with more properties as parameter for the command,
    but am going for a vertical slice first).
    
    Figured out part of the type signature with IO in it by trying some IO, leaving off
    the type signature and reading the error messages.

--- Handler/Fay.hs
+++ Handler/Fay.hs
@@ -8,9 +8,12 @@ fibs :: [Int]
 fibs = 0 : 1 : zipWith (+) fibs (drop 1 fibs)
 
 --handleBrowserFeatures :: forall t t1 t2. (t2 -> Bool -> t1) -> t -> t2 -> t1
-handleBrowserFeatures :: forall t t2 s. (t2 -> Bool -> HandlerT App IO s ) -> t -> t2 -> HandlerT App IO s
+-- The Show t on the left hand side is an example of duck typing in Haskell. As long as T implements the Show
+-- protocol it is all right (currently t is a boolean, but that is going to change quickly, and we do not need to
+-- know more here)
+handleBrowserFeatures :: forall t t2 s. Show t => (t2 -> Bool -> HandlerT App IO s ) -> t -> t2 -> HandlerT App IO s
 handleBrowserFeatures render q r = do
-  liftIO $ putStrLn "hallo"
+  liftIO $ putStrLn ("Query selector supported: " ++ (show q))
   render r $ False
                                    --  lift (liftIO $ putStrLn "Hello")
                                    --  render r $ False
