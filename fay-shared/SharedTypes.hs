82863e24af396b9329e4c534db1035255fd60863
Willem van den Ende <willem@qwan.eu>
2014-04-20 21:39:25 +0100

    WIP introduce Sum type for supported browser features
    Idea is to pair checker function with a BrowserSupportValue, and get a
    list of all supported Browser Values back.
    
    Some conditional compilation, it seems catMaybes is useful, but it does
    not exist in Fay, because it has a limited monad library.
    
    The synonym value allValues exists to get possible compile errors on
    either side.

--- fay-shared/SharedTypes.hs
+++ fay-shared/SharedTypes.hs
@@ -7,9 +7,29 @@ import Data.Data
 import Language.Fay.Yesod
 #ifdef FAY
 import FFI
+-- form Haskell Base Data-Maybe, since we don't have a complete maybe in Fay
+-- Defined conditionally so we do not pollute namespaces in Yesod
+-- TODO extract to a Fay Library, or contribute to Fay.Control.Monad
+-- | The 'catMaybes' function takes a list of 'Maybe's and returns
+-- -- a list of all the 'Just' values. 
+catMaybes :: [Maybe a] -> [a]
+catMaybes ls = [x | Just x <- ls]
 #else
 --import Language.Fay.FFI
+import Data.Maybe (catMaybes)
 #endif
+
+data BrowserSupports = AddEventListener | QuerySelector | RequestAnimationFrame | LocalStorage
+    deriving (Read, Show, Typeable, Data)
+
+data MBrowserSupports = Maybe BrowserSupports
+
+bool2Maybe :: Bool -> a -> Maybe a
+bool2Maybe True a = Just a
+bool2Maybe False _ = Nothing
+
+allValues :: [Maybe a] -> [a]
+allValues = catMaybes
 
 data Command = GetFib Int (Returns Int) |
                PostQuerySelector Bool (Returns Bool)
