340f3307043253042996da9bcbb506341e0dfb42
Willem van den Ende <willem@qwan.eu>
2014-04-24 22:57:43 +0100

    Show missing features
    Some more JS fun. I'm thinking about an alternative for the stringly
    typed programming coming up. Either create dom elements, or build a
    shadow dom (should be cool with haskell types) like Reactjs

--- fay/Home.hs
+++ fay/Home.hs
@@ -60,11 +60,28 @@ displayName = ffi "%1['instance']"
 showLi :: BrowserSupports -> String
 showLi bs = "<li>" ++ (displayName bs) ++ "</li>"
 
+
+listOf _ [] = "<p>No items in this category</p>"
+listOf fn lst = "<ul>" ++ concat (map (showLi . fn) lst) ++ "</ul>"
+
+
 -- from Data.List, but haskell version is special - inlined.
 -- this is a naive version, inspired by the comment in Data.List documentation but it works.
 -- This could work as a quickcheck property against a smarter version
 partition :: (a -> Bool) -> [a] -> ([a],[a])
 partition p xs = (filter p xs, filter (not . p) xs)
+
+displayFeatures fn lst elementId =  do
+    let htm = listOf fn lst
+    el <- getElementById elementId
+    setInnerHTML el htm
+
+displayScore checks passed elementId = do
+    let total_count = length checks
+    let passed_count = length passed
+    el <- getElementById elementId
+    let html = (show passed_count) ++ "/" ++ (show total_count)
+    setInnerHTML el html
 
 main :: Fay ()
 main = do
@@ -77,8 +94,7 @@ main = do
                   (LocalStorage, hasLocalStorage)]
 
     let (passed, failed) = partition snd checks
-    let supportedElementsLi = concat (map (showLi . fst) passed)
 
-    let htm = "<ul>" ++ supportedElementsLi ++ "</ul>"
-    browserfeaturesElement <- getElementById "detectedfeatures"
-    setInnerHTML browserfeaturesElement htm
+    displayScore checks passed "featurecount"
+    displayFeatures fst passed "detectedfeatures" -- still strings here. should be shared constants between Fay and Hamlet
+    displayFeatures fst failed "unsupportedfeatures"
